f4ed0be9a9d4cd7061db4523edb99a1ebd057176
Watheq <watheq.alshowaiter@gmail.com>
2019-10-14 15:02:04 +0300

    done vid 39 - make a show .php and display subjects based on id

--- private/query_functions.php
+++ private/query_functions.php
@@ -21,13 +21,36 @@ function find_all_subjects()
  */
 function find_all_pages()
 {
-    global $db; 
+    global $db;
 
     $sql = "SELECT  * FROM pages "; // important to leave the last space 
     $sql .= "ORDER BY subject_id, position ASC";
- // echo $sql; // just for troubleshooting 
+    // echo $sql; // just for troubleshooting 
     $result = mysqli_query($db, $sql);
-    confirm_result_set($result); 
+    confirm_result_set($result);
 
     return $result;
+}
+
+/**
+ * Find subject information from its id
+ *
+ * @param int $id
+ * @return assoc array 
+ */
+function find_subject_by_id($id)
+{
+
+    global $db; 
+
+    $sql  = "SELECT * FROM subjects ";
+    $sql .= "where id = '" . $id  . "'"; // containing 'id' with single quotes is for seacurity 
+
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+
+    $subject = mysqli_fetch_assoc($result);
+    mysqli_free_result($result); // we can free results here because it is one array not a loop $sql  = "SELECT * FROM subjects ";
+
+    return $subject; // returns assoc array not a result set  
 }
